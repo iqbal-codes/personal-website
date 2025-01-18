85f533ac53a8e61c82ea48d201543569538c4260
muhammad iqbal <muhammadiqbalks2@gmail.com>
2025-01-18 12:21:42 +0700

    feat: add some portfolio content & refactor some code for better reusable components

---
+++ src/constants/global.ts
@@ -0,0 +1,256 @@
+export const experiences = [
+  {
+    workPeriode: "OCT 2022 - PRESENT",
+    title: "Frontend Engineer · eFishery",
+    description: [
+      "Build, design, and maintain multiple websites & mobile apps to achieve 100% digitalization of the company's.",
+      "Implementing CodePush updates for React Native apps to enhance deployment effciency.",
+      "Refactor existing codebases to improve apps performance and developer experiences.",
+      "Work closely with cross-functional teams, including developers, managers, designers & products to design new features and resolve bugs.",
+      "Optimized existing React applications for improved performance and user experience.",
+    ],
+    technologies: [
+      "Javascript",
+      "Typescript",
+      "HTML & CSS",
+      "Tailwind",
+      "React",
+      "NextJS",
+      "RefineJS",
+      "React Native",
+      "Jest",
+      "Ant Design",
+      "Graphql",
+      "REST API",
+      "Zustand",
+    ],
+  },
+  {
+    workPeriode: "OCT 2020 - JUNE 2023",
+    title:
+      "Frontend Engineer > Lead Frontend Engineer · PT Pertamina Bina Medika IHC",
+    description: [
+      "Developed, Designed and maintained a high-quality user interface for a new product.",
+      "Crafted reusable React components for future use.",
+      "Took the initiative to prototype new user experiences and test them with stakeholders.",
+      "Initiate & Implemented new coding standards and best practices for the front-end team, resulting in improved effciency and a more consistent codebase across projects.",
+      "Optimized existing React applications for improved performance and user experience.",
+      "Work closely with clients to understand their needs and deliver solutions that meet their requirements.",
+    ],
+    technologies: [
+      "Javascript",
+      "Typescript",
+      "HTML & CSS",
+      "Tailwind",
+      "React",
+      "NextJS",
+      "Ant Design",
+      "REST API",
+      "React Recoil",
+    ],
+  },
+  {
+    workPeriode: "APR 2019 - AUG 2022",
+    title: "Frontend Engineer · Virtual Spirit",
+    description: [
+      "Mainly focused on build & maintain high-quality web and mobile applications.",
+      "Designed and implemented a user-friendly interface for the app.",
+      "Optimized the app for performance and user experience.",
+      "Collaborated with cross-functional teams to deliver high-quality mobile apps.",
+    ],
+    technologies: [
+      "Javascript",
+      "Typescript",
+      "HTML & CSS",
+      "React",
+      "React Native",
+      "REST API",
+      "React Redux",
+      "Redux Saga",
+    ],
+  },
+  {
+    workPeriode: "OCT 2018 - APR 2019",
+    title: "Frontend Engineer · Komers",
+    description: [
+      "Joined a very early-stage startup as a fresh graduate.",
+      "Built e-commerce apps from scratch using React Native and React.js in a team of three engineers.",
+      "Designed and implemented user interfaces without a dedicated UI/UX designer.",
+      "Delivered core features like product listings, carts, and payments while ensuring scalable and maintainable code.",
+    ],
+    technologies: [
+      "Javascript",
+      "Typescript",
+      "HTML & CSS",
+      "React",
+      "React Native",
+      "REST API",
+      "React Redux",
+      "Redux Saga",
+    ],
+  },
+];
+
+export const projects = [
+  {
+    name: "Crowdver Tool, eFishery",
+    description:
+      "Develop & maintain Crowdver Tool, a Progressive Web App to manage farmer verification orders to decrease the number of fraud by increasing the number of verified farmers.",
+    technologies: [
+      "Typescript",
+      "NextJS",
+      "RefineJS",
+      "REST API",
+      "react-query",
+      "Ant Design",
+      "Jest",
+      "PWA",
+    ],
+  },
+  {
+    name: "CRM Dashboard, eFishery",
+    description:
+      "Develop & maintain CRM Dashboard used by eFishery to manage all customer related data, such as customer profile, customer order, customer transaction, etc.",
+    technologies: [
+      "Typescript",
+      "CSS",
+      "NextJS",
+      "react-query",
+      "code-push",
+      "bitbucket pipelines",
+      "Ant Design",
+      "Jest",
+    ],
+  },
+  {
+    name: "Sales Diary, eFishery",
+    description:
+      "Develop & maintain Sales Field Productivity Apps named Sales Diary used by Sales & Field Teams for all activities, such as supply management, order management, team reporting, etc.",
+    technologies: [
+      "Typescript",
+      "CSS",
+      "React Native",
+      "react-query",
+      "code-push",
+      "bitbucket pipelines",
+    ],
+  },
+  {
+    name: "Fish Internal Tool, eFishery",
+    description:
+      "Internal administration named Fish Internal Tool used by the internal team for all Fish Business Unit team for administrative activities.",
+    technologies: [
+      "Typescript",
+      "CSS",
+      "NextJS",
+      "Refine.Js",
+      "react-query",
+      "zustand",
+      "Ant Design",
+      "Jest",
+    ],
+  },
+  {
+    name: "eFisheryku, eFishery",
+    description:
+      "Membership apps named eFisheryku used by members of eFishery to sell commodities, buy feed, etc. in 1 app, also Members can use kabayan to apply for a loan.",
+    technologies: [
+      "Typescript",
+      "CSS",
+      "React.Js",
+      "Vite",
+      "graphql",
+      "react-query",
+      "zustand",
+      "Jest",
+    ],
+  },
+  {
+    name: "Medinesia, Pertamedika",
+    description:
+      "Hospital Information System Web Apps named Medinesia used by Pertamina Hospital that allows the collection of all data for the treatment of patients & maintain the data for future providers.",
+    technologies: [
+      "Javascript",
+      "CSS",
+      "React.Js",
+      "react-query",
+      "redux",
+      "recoil",
+      "Ant Design",
+    ],
+  },
+  {
+    name: "SIRS Admin, Pertamedika",
+    description:
+      "Admin Apps named Sirs Admin used by Pertamina Hospital allows users to manage all data that is used in Medinesia Apps.",
+    technologies: [
+      "Typescript",
+      "CSS",
+      "Next.Js",
+      "react-query",
+      "recoil",
+      "Ant Design",
+    ],
+  },
+  {
+    name: "Circles.MD, Virtual Spirit",
+    description:
+      "Mobile communication apps named Circles.Md that had a mission to improve the working lives of health professionals and promote better patient care in hospitals.",
+    technologies: [
+      "Typescript",
+      "CSS",
+      "Google API",
+      "PubNub",
+      "React Native",
+      "redux saga",
+    ],
+  },
+  {
+    name: "PropSocial, Virtual Spirit",
+    description:
+      "Mobile property apps named PropSocial to feature honest reviews direct from the community, with an easy-to-use interface that will help users find their ideal property within minutes.",
+    technologies: [
+      "Javascript",
+      "CSS",
+      "MapBox API",
+      "React Native",
+      "redux saga",
+    ],
+  },
+  {
+    name: "Updated, Virtual Spirit",
+    description:
+      "Develop from scratch mobile social media apps named Updated that focused on photos and videos sharing like Instagram.",
+    technologies: [
+      "Javascript",
+      "CSS",
+      "Google API",
+      "React Native",
+      "Firebase",
+      "redux saga",
+    ],
+  },
+  {
+    name: "Virtualspace, Virtual Spirit",
+    description:
+      "Develop & maintain mobile & Web team collaboration apps named VirtualSpace that mission to improve the working lives & communications of organizations, have features to discuss with teams, also have kanban task management to manage team tasks.",
+    technologies: [
+      "Javascript",
+      "CSS",
+      "Google API",
+      "React Js",
+      "PubNub",
+      "React Native",
+      "redux saga",
+    ],
+  },
+  {
+    name: "Komers App, Komers",
+    description:
+      "Develop & Design from scratch mobile e-commerce apps that focus on selling textile components like cloth, thread, etc.",
+    technologies: ["JavaScript", "React Native", "CSS", "Redux Saga"],
+  },
+];
+
+export const segments = ["ABOUT", "EXPERIENCES", "PROJECTS"];
+
